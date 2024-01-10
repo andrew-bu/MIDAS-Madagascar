5c3750e0dafb86f5d6f9041202893a77ae4bf38b
andrew-bu <97057116+andrew-bu@users.noreply.github.com>
2024-01-10 15:13:06 -0500

    First test setup

---
+++ Core_MIDAS_Code/Agent.m
@@ -0,0 +1,100 @@
+classdef Agent < handle
+    
+   properties 
+       %agent properties
+       id
+       location
+       matrixLocation
+       visX
+       visY
+       wealth
+       wealthHistory
+       realizedUtility
+       age
+       gender
+       TOD
+       DOB
+       trapped
+       
+       %agent accumulated data
+       network
+       myIndexInNetwork
+       accessCodesPaid
+       bestPortfolios
+       bestAspirations
+       consideredPortfolios
+       consideredHistory
+       bestFidelity
+       bestPortfolioValues
+       knowsIncomeLocation
+       incomeLayersHistory
+       training
+       experience
+       scratch;
+       overlap
+       heardOpening
+       expectedProbOpening
+       timeProbOpeningUpdated
+       %incomeLayersTest
+       currentPortfolio
+       currentAspiration
+       currentFidelity
+       firstPortfolio
+       agentPortfolioHistory
+       agentAspirationHistory
+       personalIncomeHistory
+       currentSharedIn
+       lastIntendedShareIn
+       moveHistory
+
+       %agent preferences
+       incomeShareFraction
+       shareCostThreshold
+       knowledgeShareFrac
+       pInteract
+       pMeetNew
+       pAddFitElement
+       pChoose
+       pBackCast
+       fDecay
+       pGetLayer_informed
+       pGetLayer_uninformed
+       pRandomLearn
+       countRandomLearn
+       numBestLocation
+       numBestPortfolio
+       numRandomLocation
+       numRandomPortfolio
+       numPeriodsEvaluate
+       numPeriodsMemory
+       discountRate
+       rValue
+       bList
+       prospectLoss
+   end
+   
+   events
+      %none at the moment
+   end
+   
+   methods 
+       %basic constructor
+      function A = Agent(id, location)
+         A.id = id;
+         A.location = location;
+         A.network = [];
+         A.TOD = -9999;  %TOD is 'time of death'
+         A.DOB = -9999;  %DOB is 'date of birth'
+         A.trapped = 0;
+      end %  
+      
+      %as written presently, most agent actions are coded as model
+      %subroutines with input agents, as opposed to agent member functions
+      
+      %since there are no child classes that inherit Agent, there isn't
+      %really much of a need, and this structure makes it easier to plug
+      %and play different routines
+     
+   end % methods
+   
+end % classdef
